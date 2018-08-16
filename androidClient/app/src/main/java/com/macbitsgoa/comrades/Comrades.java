acffa947bc9d12f66e5723377b7b5e3aa4dbb0cb
aayushsingla <f20170815@goa.bits-pilani.ac.in>
2018-08-16 20:15:32 +0530

    fixed Codacy Issues

--- androidClient/app/src/main/java/com/macbitsgoa/comrades/Comrades.java
+++ androidClient/app/src/main/java/com/macbitsgoa/comrades/Comrades.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.preference.PreferenceManager;
+import android.util.Log;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -14,6 +15,7 @@ import com.google.firebase.database.ValueEventListener;
 import com.macbitsgoa.comrades.courselistfragment.MyCourse;
 import com.macbitsgoa.comrades.persistance.Database;
 
+import static com.macbitsgoa.comrades.CHCKt.TAG_PREFIX;
 import static com.macbitsgoa.comrades.HomeActivity.SETTINGS;
 
 /**
@@ -24,30 +26,30 @@ public class Comrades extends Application {
 
     @SuppressLint("StaticFieldLeak")
     private static Context context;
-
+    private final String TAG = TAG_PREFIX + Comrades.class.getSimpleName();
     @Override
     public void onCreate() {
         super.onCreate();
         Comrades.context = getApplicationContext();
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
         Boolean coursesPresent = preferences.getBoolean("Courses Present", false);
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
         if (!coursesPresent) {
+            FirebaseDatabase.getInstance().getReference(BuildConfig.BUILD_TYPE).child("courses")
+                    .addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            FetchAllCourses fetchAllCourses = new FetchAllCourses();
+                            fetchAllCourses.execute(dataSnapshot);
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+                            Log.e(TAG, databaseError.getMessage());
+                        }
+                    });
 
         }
-        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
-        FirebaseDatabase.getInstance().getReference(BuildConfig.BUILD_TYPE).child("courses")
-                .addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        FetchAllCourses fetchAllCourses = new FetchAllCourses();
-                        fetchAllCourses.execute(dataSnapshot);
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-
-                    }
-                });
 
     }
 
